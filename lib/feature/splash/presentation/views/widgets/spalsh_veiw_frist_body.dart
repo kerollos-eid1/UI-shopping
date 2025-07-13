71378256b3495fc75ec9bb130d05f79285733ec8
unknown <kerolloseid34@gmail.com>
2025-07-13 17:01:56 +0300

    update file splash view frist body

--- lib/feature/splash/presentation/views/widgets/spalsh_veiw_frist_body.dart
+++ lib/feature/splash/presentation/views/widgets/spalsh_veiw_frist_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:ui_shop/feature/splash/presentation/views/screens/last_splash_view.dart';
+
 import 'package:ui_shop/feature/splash/presentation/views/widgets/On_Boarding_Footer.dart';
 
 import 'package:ui_shop/feature/splash/presentation/views/widgets/custom_app_Bar.dart';
