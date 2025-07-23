920c34bcf5b677f77fb149a60ac24886c73cecf0
unknown <kerolloseid34@gmail.com>
2025-07-23 21:45:54 +0300

    Added file filter and sort to folder home

---
+++ lib/feature/home/presentation/views/widgets/filter_sort.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ui_shop/core/utils/style.dart';
+
+class SortFilter extends StatelessWidget {
+  const SortFilter({super.key, required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(text, style: Styles.textStyles18),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.3),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              backgroundColor: const Color.fromARGB(255, 235, 228, 228),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Sort', style: TextStyle(color: Colors.black)),
+                SizedBox(width: 4),
+                Icon(
+                  FontAwesomeIcons.arrowDownShortWide,
+                  color: Colors.black,
+                  size: 20,
+                ),
+              ],
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              backgroundColor: const Color.fromARGB(255, 235, 228, 228),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Filter', style: TextStyle(color: Colors.black)),
+                SizedBox(width: 4),
+                Icon(Icons.filter_alt, color: Colors.black, size: 20),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/feature/home/presentation/views/widgets/home_page_view.dart
+++ lib/feature/home/presentation/views/widgets/home_page_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:ui_shop/core/utils/style.dart';
 import 'package:ui_shop/feature/home/presentation/views/widgets/Custom_app_bar_view.dart';
 import 'package:ui_shop/feature/home/presentation/views/widgets/Search_sceen.dart';
+import 'package:ui_shop/feature/home/presentation/views/widgets/filter_sort.dart';
 
 class HomePageVeiw extends StatelessWidget {
   const HomePageVeiw({super.key});
@@ -15,58 +14,6 @@ class HomePageVeiw extends StatelessWidget {
         SearchScreen(),
         SortFilter(text: 'All Feature'),
       ],
-    );
-  }
-}
-
-class SortFilter extends StatelessWidget {
-  const SortFilter({super.key, required this.text});
-  final String text;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(text, style: Styles.textStyles18),
-          SizedBox(width: MediaQuery.of(context).size.width * 0.3),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              backgroundColor: const Color.fromARGB(255, 235, 228, 228),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Sort', style: TextStyle(color: Colors.black)),
-                SizedBox(width: 4),
-                Icon(
-                  FontAwesomeIcons.arrowDownShortWide,
-                  color: Colors.black,
-                  size: 20,
-                ),
-              ],
-            ),
-          ),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              padding: EdgeInsets.symmetric(horizontal: 12),
-              backgroundColor: const Color.fromARGB(255, 235, 228, 228),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Filter', style: TextStyle(color: Colors.black)),
-                SizedBox(width: 4),
-                Icon(Icons.filter_alt, color: Colors.black, size: 20),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
